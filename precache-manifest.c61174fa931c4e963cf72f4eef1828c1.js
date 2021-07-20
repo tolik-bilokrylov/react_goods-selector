dd775de6995872e5b713dc18aa71cc0bb40ba8fc
Tolik Bilokrylov <enkorn1715@gmail.com>
2021-07-20 15:41:06 +0300

    Updates

---
+++ precache-manifest.c61174fa931c4e963cf72f4eef1828c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa46cf4fe1de73b94af3f017f0676fc3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8fdeef65e93dbd028b37",
+    "url": "./static/css/main.81baf298.chunk.css"
+  },
+  {
+    "revision": "e91e992495cd4a2edcf5",
+    "url": "./static/js/2.6b08981b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.6b08981b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fdeef65e93dbd028b37",
+    "url": "./static/js/main.f1072264.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  },
+  {
+    "revision": "b55c9de030325ee782b2e33d5ecb4509",
+    "url": "./static/media/ball.b55c9de0.png"
+  },
+  {
+    "revision": "82f5b5972ae585eafba794f0a6330eba",
+    "url": "./static/media/cBall.82f5b597.png"
+  }
+]);
